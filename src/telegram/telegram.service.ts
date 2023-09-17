84a3dc4db33395b85cbf82a1c4aa0fdda777c6b1
Danhalf <danhel@rocketmail.com>
2023-09-17 21:11:32 +0300

    added: api

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
+import { Scenes, Telegraf } from 'telegraf';
 
-@Injectable()
-export class TelegramService {}
+type Context = Scenes.SceneContext;
+
+@Update()
+export class TelegramService extends Telegraf<Context> {
+    @Start()
+    onStart(@Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<b>Hello ${ctx.from.first_name} ${ctx.from.last_name}</b> I'm ready to serve.`);
+    }
+
+    @On('text')
+    onMessage(@Message('text') message: string, @Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<i>MESSAGE: ${message}</i>`);
+    }
+}
